f03d0b1d71367fa011f7d4ab13234f59350e96d6
VeryGoodDev <verygooddev@outlook.com>
2020-07-07 02:15:46 -0600

    Add some metadata to commands

--- chatbot/commands.js
+++ chatbot/commands.js
@@ -4,27 +4,33 @@ const commands = {
   // Static simple responses
   '!bot': {
     handler(sender, respond) {
-      respond(`Dev decided to write his own bot. Still a work in progress.`)
+      respond(
+        `Dev decided to write his own bot. Still a work in progress. Bugs happening during stream always a distinct possibility.`
+      )
     },
+    description: `Use to get a little bit of info about KindaOkayBot`,
   },
   '!hydrate': {
     handler(sender, respond) {
       respond(`Pssst. Hey you. Drinking water is cool, you should do that.`)
     },
+    description: `Use to send a friendly reminder to drink some earth juice`,
   },
   '!minecraft': {
     handler(sender, respond) {
       respond(
-        `Dev has goals for the Minecraft survival world! These include building three villages in three biomes and making an excessive amount of automated farms. More detailed info coming soon!`
+        `Dev has goals for the Minecraft survival world! These include creating three villages in three biomes, making an excessive amount of automated farms, and building a museum to display every block and item in the game. More detailed info coming soon!`
       )
     },
+    description: `Use to see a summary of some of Dev's goals/plans with his Minecraft survival world`,
   },
   '!schedule': {
     handler(sender, respond) {
       respond(
-        `Dev usually streams 4-6 times a week, usually in the evening. No set-in-stone schedule for now, but feel free to follow to receive notifications whenever Dev goes live!`
+        `Dev usually streams 4-5 times a week, usually in the evening. Normally live Tuesday through Thursday, and at least one day out of Saturday and Sunday (and both when able!). Not set in stone since IRL stuff occasionally gets in the way, but followed as often as possible`
       )
     },
+    description: `Use to see Dev's usual schedule for going live`,
   },
   '!twitter': {
     handler(sender, respond) {
@@ -32,6 +38,7 @@ const commands = {
         `Follow Dev on Twitter for stream-related updates and occasional random thoughts! https://twitter.com/_verygooddev (the username without an underscore was already taken by someone who hasn't even tweeted since 2011 :/)`
       )
     },
+    description: `Use to get a link to Dev's Twitter account, along with some mild sass towards whoever has the Twitter handle Dev wanted`,
   },
   // Dynamic simple responses
   '!beans': {
@@ -45,6 +52,7 @@ const commands = {
     hasPermission(sender) {
       return [`theallbean05`, `loubob05`].includes(sender.username)
     },
+    exclusive: true,
   },
   '!creepylurk': {
     handler(sender, respond) {
@@ -52,6 +60,7 @@ const commands = {
         `${sender.displayName} is lurking. Not like a normal lurk though, no no no. Instead, ${sender.displayName} is up in a tree, hiding behind the branches and leaves, and watching from the distance through binoculars while chuckling quietly at their frankly disturbing lurking behavior. Thanks for the lurk?`
       )
     },
+    description: `Use to declare your intent to lurk in the creepiest way possible`,
   },
   '!gamelurk': {
     handler(sender, respond, ...args) {
@@ -60,6 +69,7 @@ const commands = {
         `${sender.displayName} wants to chill in the stream, but they also want to play ${game}. Both? Yeah, both is good. Have fun with the game, and happy lurking!`
       )
     },
+    description: `Use to declare your intent to lurk while playing a game. The game to be played can optionally be specified e.g. \`!gamelurk minecraft\``,
   },
   '!lurk': {
     handler(sender, respond) {
@@ -67,6 +77,7 @@ const commands = {
         `${sender.displayName} is lurking in the shadows, still watching but now doing so from a distance. Happy lurking!`
       )
     },
+    description: `The generic lurk command when none of the more specific ones fit. Lurkers always welcome!`,
   },
   '!sleepylurk': {
     handler(sender, respond) {
@@ -74,6 +85,7 @@ const commands = {
         `As ${sender}'s laying down to sleep / Within the stream they stay to creep. Have a good sleep HahaSleep thanks for the lurk!`
       )
     },
+    description: `Use to declare your intent to go to bed, but still leave the stream up as a lurker`,
   },
   '!so': {
     handler(sender, respond, streamerParam) {
@@ -88,6 +100,8 @@ const commands = {
     hasPermission(sender) {
       return sender.mod === true
     },
+    permissionLevel: `mods`,
+    description: `Use to show some love for fellow streamers. Mod only to avoid the potential for abuse (attempts to use the command if you're not a mod will result in warnings or timeouts)`,
   },
   '!stilllurking': {
     handler(sender, respond) {
@@ -95,11 +109,13 @@ const commands = {
         `${sender.displayName} has been lurking and just popped by to provide an update: they are still lurking. We appreciate this important update.`
       )
     },
+    description: `Use to check in while lurking but before you're able to hang out in chat`,
   },
   '!unlurk': {
     handler(sender, respond) {
       respond(`${sender.displayName} has come back from watching in the shadows.`)
     },
+    description: `Use to declare that your lurk is over and you're hanging out in chat again`,
   },
   '!worklurk': {
     handler(sender, respond) {
@@ -107,6 +123,7 @@ const commands = {
         `${sender.displayName} has to work, but was kind enough to stop by the stream and have it up in the background while they work. Hope that work goes well, and happy lurking!`
       )
     },
+    description: `Use to declare your intent to lurk in the stream while you're working (Dev does this often while doing his full time job and appreciates anyone who is able to lurk while working)`,
   },
   // Not simple responses
   '!uptime': {
@@ -125,6 +142,7 @@ const commands = {
         }
       })
     },
+    description: `Use to find out how long Dev has been live`,
   },
 }
 
