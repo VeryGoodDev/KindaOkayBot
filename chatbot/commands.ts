248ba28ec2ac53923bd6a664422d5c8831331a74
VeryGoodDev <very_good_dev@outlook.com>
2022-05-10 17:47:06 -0700

    Add a couple more commands

--- chatbot/commands.ts
+++ chatbot/commands.ts
@@ -20,6 +20,18 @@ const commandMap: Record<Command, CommandData> = {
       return `Dev's channel points are called bytes. There aren't a lot of custom ones right now, so if you have any cool ideas, feel free to share in chat for Dev to consider!`
     },
   },
+  '!commands': {
+    description: `Provides a link to a webpage that lists all commands supported by KindaOkayBot`,
+    getResponse() {
+      return `Coming soon (feel free to ask Dev if you're looking for anything specific)`
+    },
+  },
+  '!discord': {
+    description: `Provides an invite link for Dev's Discord server`,
+    getResponse() {
+      return `https://discord.gg/XqSQaC3`
+    },
+  },
 }
 
 export default commandMap
