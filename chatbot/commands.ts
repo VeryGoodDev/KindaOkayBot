cb2a88f808a20f03fbd0253bcabe93621f4f1e4a
VeryGoodDev <very_good_dev@outlook.com>
2022-05-15 11:30:53 -0700

    Add a few more lurk commands

--- chatbot/commands.ts
+++ chatbot/commands.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention -- Keys in commandMap specifically don't follow this rule */
 import { customizeResponse, INTERACTION, SHOUTOUT_DESCRIPTORS } from './commandResponseHelpers'
-import { getDisplayName } from './userHelpers'
+import { getDisplayName, getUsername } from './userHelpers'
 import { chooseRandom, getCommaSeparatedList } from './util'
 
 import type { Userstate } from 'tmi.js'
@@ -82,12 +82,20 @@ const staticCommands: CommandMap = {
   },
 }
 
-const simpleDynamicCommands: CommandMap = {
+const lurkCommands: CommandMap = {
   '!creepylurk': {
     description: `Use to declare your intent to lurk in the creepiest way possible`,
     getResponse(userState) {
       const displayName = getDisplayName(userState)
       return `${displayName} is lurking. Not like a normal lurk though, no no no. Instead, ${displayName} is up in a tree, hiding behind the branches and leaves, and watching from the distance through binoculars while chuckling quietly at their frankly disturbing lurking behavior. Thanks for the lurk?`
+    },
+  },
+  '!dishwashinglurk': {
+    description: `Use to declare your intent to lurk while washing some dishes`,
+    getResponse(userState) {
+      return `${getDisplayName(
+        userState
+      )} has some dishes that they need to wash, but is still leaving the stream up in lurk mode. May your dishes be easy to clean, and thanks for the lurk!`
     },
   },
   '!drivelurk': {
@@ -106,43 +114,21 @@ const simpleDynamicCommands: CommandMap = {
       )} wants to chill in the stream, but they also want to play ${game}. Both? Yeah, both is good. Have fun with your game, and thanks for the lurk!`
     },
   },
-  '!grouphug': {
-    description: `Use to give a virtual group hug to everyone in chat`,
-    getResponse(userState) {
-      return `${getDisplayName(userState)} gave a big ol group hug to the entire chat! <3`
-    },
-  },
-  '!highfive': {
-    description: `Use to give a high five to one or more people in chat`,
-    getResponse(userState, ...recipients) {
-      const recipient = recipients.length > 0 ? getCommaSeparatedList(recipients) : `a random person in chat`
-      return customizeResponse(INTERACTION, {
-        displayName: getDisplayName(userState),
-        emote: `:D`,
-        interaction: `a perfect high five`,
-        recipient,
-      })
-    },
-  },
-  '!hug': {
-    description: `Use to give a virtual hug to one or more people in chat`,
-    getResponse(userState, ...recipients) {
-      // TODO hug strength/judgment (at least for one recipient, maybe can do for multiple)
-      const recipient = recipients.length > 0 ? getCommaSeparatedList(recipients) : `a random person in chat`
-      return customizeResponse(INTERACTION, {
-        displayName: getDisplayName(userState),
-        emote: `<3`,
-        interaction: `a hug`,
-        recipient,
-      })
-    },
-  },
   '!lurk': {
     description: `Use to declare your intent to go into lurker mode`,
     getResponse(userState) {
       return `${getDisplayName(
         userState
       )} is lurking in the shadows, still in chat but now from a distance. Thanks for the lurk!`
+    },
+  },
+  '!movielurk': {
+    description: `Use to declare your intent to lurk while watching a movie (or TV show). You can optionally specify what you'll be watching if you want, e.g. \`!movielurk the last jedi\``,
+    getResponse(userState, ...args) {
+      const title = args.length > 0 ? args.join(` `) : `a sick-ass movie or TV show`
+      return `${getUsername(
+        userState
+      )} wants to chill in the stream, but they're also in the mood to watch ${title}. Both? Both. Enjoy whatever it is you're watching, and thanks for the lurk!`
     },
   },
   '!sleepylurk': {
@@ -152,18 +138,6 @@ const simpleDynamicCommands: CommandMap = {
         userState
       )} lays down to sleep / Within the stream they stay to creep. Have a good sleep HahaSleep thanks for the lurk!`
     },
-  },
-  '!so': {
-    description: `Use to put a shoutout in chat for a fellow streamer`,
-    getResponse(userState, streamerParam) {
-      if (!streamerParam) {
-        return ``
-      }
-      const streamer = streamerParam.replace(/^@/, ``)
-      const descriptor = chooseRandom(SHOUTOUT_DESCRIPTORS)
-      return `Shout out to the ${descriptor} ${streamer}! Go show them some love at https://twitch.tv/${streamer.toLowerCase()}`
-    },
-    restrictUsage: `MOD`,
   },
   '!sortalurk': {
     description: `Use to declare your intent to still be in and out of chat, but not quite fully lurking`,
@@ -199,6 +173,52 @@ const simpleDynamicCommands: CommandMap = {
   },
 }
 
+const simpleDynamicCommands: CommandMap = {
+  '!grouphug': {
+    description: `Use to give a virtual group hug to everyone in chat`,
+    getResponse(userState) {
+      return `${getDisplayName(userState)} gave a big ol group hug to the entire chat! <3`
+    },
+  },
+  '!highfive': {
+    description: `Use to give a high five to one or more people in chat`,
+    getResponse(userState, ...recipients) {
+      const recipient = recipients.length > 0 ? getCommaSeparatedList(recipients) : `a random person in chat`
+      return customizeResponse(INTERACTION, {
+        displayName: getDisplayName(userState),
+        emote: `:D`,
+        interaction: `a perfect high five`,
+        recipient,
+      })
+    },
+  },
+  '!hug': {
+    description: `Use to give a virtual hug to one or more people in chat`,
+    getResponse(userState, ...recipients) {
+      // TODO hug strength/judgment (at least for one recipient, maybe can do for multiple)
+      const recipient = recipients.length > 0 ? getCommaSeparatedList(recipients) : `a random person in chat`
+      return customizeResponse(INTERACTION, {
+        displayName: getDisplayName(userState),
+        emote: `<3`,
+        interaction: `a hug`,
+        recipient,
+      })
+    },
+  },
+  '!so': {
+    description: `Use to put a shoutout in chat for a fellow streamer`,
+    getResponse(userState, streamerParam) {
+      if (!streamerParam) {
+        return ``
+      }
+      const streamer = streamerParam.replace(/^@/, ``)
+      const descriptor = chooseRandom(SHOUTOUT_DESCRIPTORS)
+      return `Shout out to the ${descriptor} ${streamer}! Go show them some love at https://twitch.tv/${streamer.toLowerCase()}`
+    },
+    restrictUsage: `MOD`,
+  },
+}
+
 const channelPointRedemptionCommands: CommandMap = {
   '!incagbreto': {
     description: ``,
@@ -216,6 +236,7 @@ const twitchApiCommands: CommandMap = {}
 
 const commandMap = {
   ...staticCommands,
+  ...lurkCommands,
   ...simpleDynamicCommands,
   ...channelPointRedemptionCommands,
   ...quoteCommands,
@@ -228,10 +249,13 @@ const commandAliases: Record<Command, Command> = {
   '!channelpoints': `!bytes`,
   '!creeperlurk': `!creepylurk`,
   '!creeplurk': `!creepylurk`,
+  '!disheslurk': `!dishwashinglurk`,
+  '!dishwasherlurk': `!dishwashinglurk`,
   '!points': `!bytes`,
   '!shoutout': `!so`,
   '!sleeplurk': `!sleepylurk`,
   '!stillurking': `!stilllurking`,
+  '!tvlurk': `!movielurk`,
   '!werklurk': `!worklurk`,
   '!wurklurk': `!worklurk`,
 }
@@ -241,10 +265,11 @@ export {
   PermissionLevels,
   ResponseTypes,
   staticCommands,
+  lurkCommands,
   simpleDynamicCommands,
-  channelPointRedemptionCommands,
   quoteCommands,
   twitchApiCommands,
+  channelPointRedemptionCommands,
   commandAliases,
 }
 export type { Command, CommandData }
