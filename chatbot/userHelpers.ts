d31e8ae32a4520a6b14a22a0d2a45634e94e2b4d
VeryGoodDev <very_good_dev@outlook.com>
2022-05-15 11:18:21 -0700

    Documentated a couple utils

--- chatbot/userHelpers.ts
+++ chatbot/userHelpers.ts
@@ -2,6 +2,9 @@ import { niceJson } from './util'
 
 import type { Userstate } from 'tmi.js'
 
+/**
+ * Gets the username from the provided user state. Guaranteed to always be lowercase, for convenience in comparisons etc.
+ */
 export const getUsername = (userState: Userstate): string => {
   if (typeof userState.username === `string`) {
     return userState.username.toLowerCase()
@@ -11,6 +14,9 @@ export const getUsername = (userState: Userstate): string => {
   return `<NO_NAME_FOUND>`
 }
 
+/**
+ * Gets the user's defined display name (custom capitalization) if available, returns the username if not.
+ */
 export const getDisplayName = (userState: Userstate): string => {
   if (typeof userState[`display-name`] === `string`) {
     return userState[`display-name`]
