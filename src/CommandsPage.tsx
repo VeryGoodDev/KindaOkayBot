8151bed945814926d27ee6a8e8a0a513f073040c
VeryGoodDev <very_good_dev@outlook.com>
2022-05-18 22:37:40 -0700

    Add commands imports to start playing with soon

--- src/CommandsPage.tsx
+++ src/CommandsPage.tsx
@@ -1,3 +1,35 @@
-const CommandsPage = () => <div>Command list soon</div>
+import { css } from '@emotion/css'
+
+import {
+  staticCommands,
+  lurkCommands,
+  simpleDynamicCommands,
+  quoteCommands,
+  twitchApiCommands,
+  commandAliases,
+} from '../chatbot/commands'
+
+import type { CommandMap } from '../chatbot/commands'
+
+const allCommands: CommandMap = {
+  ...staticCommands,
+  ...lurkCommands,
+  ...simpleDynamicCommands,
+  ...quoteCommands,
+  ...twitchApiCommands,
+}
+
+const placeholderCss = css`
+  display: grid;
+  font-size: 1.25rem;
+  padding-block-start: 16px;
+  place-items: center;
+`
+
+const CommandsPage = () => {
+  console.log(allCommands)
+  console.log(commandAliases)
+  return <div class={placeholderCss}>Command list coming soon</div>
+}
 
 export default CommandsPage
