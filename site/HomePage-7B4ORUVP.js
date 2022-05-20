0118cee6062d4fcbd67f829cb8198fd927f072b1
VeryGoodDev <very_good_dev@outlook.com>
2022-05-19 21:56:06 -0700

    New build

--- site/HomePage-7B4ORUVP.js
+++
@@ -1,7 +0,0 @@
-import{b as e,j as s}from"./chunk-XIKEZXWT.js";var o=s`
-  display: grid;
-  font-family: Righteous, sans-serif;
-  font-size: 3rem;
-  height: 100%;
-  place-items: center;
-`,i=()=>e("div",{class:o},"Beep boop"),a=i;export{a as default};
